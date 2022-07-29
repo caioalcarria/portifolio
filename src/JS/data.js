92f50eec0138b4574cd7e8d23a923b44ea31b5b7
caio <alcarriacaio@gmail.com>
2022-07-28 21:09:46 -0300

    preliminar

---
+++ src/JS/data.js
@@ -0,0 +1,38 @@
+let projetos = [
+    {
+        img: "./src/img/kenzieflix.png",
+        title: "kenzieflix",
+        description:"Pagina inicial de plataforma de streaming",
+        link:"https://kenzie-academy-brasil-developers.github.io/m1-sprint_2-kenzieflix-caioalcarria/"
+    },
+    {
+        img: "./src/img/portifolio.png",
+        title: "Portifólio",
+        description:"Landing page passoal",
+        link:"https://caioalcarria.github.io/portifolio/"
+    },
+    {
+        img: "./src/img/E-comerce.png",
+        title: "E-comerce",
+        description:"catálogo loja virtual",
+        link:"https://kenzie-academy-brasil-developers.github.io/m1-s1-e-commerce-com-flexbox-caioalcarria/"
+    },
+    {
+        img: "./src/img/Em breve.png",
+        title: "Em breve",
+        description:"Em breve novos projetos",
+        link:""
+    },
+    {
+        img: "./src/img/Em breve.png",
+        title: "Em breve",
+        description:"Em breve novos projetos",
+        link:""
+    },
+    {
+        img: "./src/img/Em breve.png",
+        title: "Em breve",
+        description:"Em breve novos projetos",
+        link:""
+    }
+]
